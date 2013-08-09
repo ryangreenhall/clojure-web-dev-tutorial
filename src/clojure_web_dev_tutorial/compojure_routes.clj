ec33944a0c2ce4a43c28cc03f17e209de67a902c
Jon Neale <jon.neale@uswitch.com>
2013-08-09 09:43:24 +0100

    Moved out param queries requiring middleware into a separate example

--- src/clojure_web_dev_tutorial/compojure_routes.clj
+++ src/clojure_web_dev_tutorial/compojure_routes.clj
@@ -14,27 +14,13 @@
     [name]
     (str "Hello " name))
 
-  ;;http://localhost:8080/greet/jon
-  ;;Query string example - needs middleware
-  (GET "/greeting"
-    [salutation name]
-    (format "Hello %s %s" salutation name))
-
   (GET "/error-name"
     []
     {:status 500
-     :body "Sorry, there has been a problem. Please try again with a different name"})
+     :body "Sorry, there has been a problem. Please try again with a different name"}))
 
-  (GET "/greeting.json"
-    [salutation name]
-    (let [name (format "Hello %s %s" salutation name)]
-      {:status 200
-       :headers {"Content-Type"   "application/json"
-                 "Vary"           "Accept-Encoding"
-                 "Content-Length" (str (.length name))}
-       :body (json-str {:greetings
-                        {:salutation salutation
-                         :name       name}})})))
+
+
 
 (defn -main [& args]
   (println (str "Starting the switch-api on port 8080...."))
