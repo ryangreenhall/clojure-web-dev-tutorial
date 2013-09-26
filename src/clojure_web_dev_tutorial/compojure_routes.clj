b172d5a0156b93d1e15d21c54f3c37fec310c497
jonneale <jonneale@gmail.com>
2013-09-26 20:00:28 +0100

    Added solutions to exercise 1

--- src/clojure_web_dev_tutorial/compojure_routes.clj
+++ src/clojure_web_dev_tutorial/compojure_routes.clj
@@ -7,17 +7,28 @@
 
   ;; 1. Define a route which responds to requests to "/" and returns a response with the body set to "hello world"
 
+  ;;http://localhost:8080
+  (GET "/"
+    []
+    "Hello World")
 
   ;; 2. Define a route which responds to /greet/(any-name) where any name is a url parameter, and returns a response
   ;;    with the body set to a "Hello (any-name)",
   ;;    e.g. request  -> /greet/jon
   ;;         response -> "hello jon"
 
+  ;;http://localhost:8080/greet/jon
+  (GET "/greet/:name"
+    [name]
+    (str "Hello " name))
+
 
   ;; 3. Define a route which responds to /error-name which returns a status code of 500 and a body containing a friendly
   ;;    error message
-
-)
+  (GET "/error-name"
+    []
+    {:status 500
+     :body "Sorry, there has been a problem. Please try again with a different name"}))
 
 
 (defn -main [& args]
