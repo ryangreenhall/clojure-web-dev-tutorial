624c53e807a300a82b8af2bd92b37fe7d0964da1
Ryan Greenhall <ryan.greenhall@gmail.com>
2013-09-29 15:19:08 +0100

    Adding some tasks for people to work through during the tutorial

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "Hands on introduction to web development in Clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-core "1.2.0"]
-                 [ring/ring-jetty-adapter "1.1.0"]]
-                 
+                 [ring/ring-jetty-adapter "1.1.0"]]            
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler clojure-web-dev-tutorial.ring.intro/handler})
--- src/clojure_web_dev_tutorial/ring/intro.clj
+++ src/clojure_web_dev_tutorial/ring/intro.clj
@@ -1,11 +1,20 @@
 (ns clojure-web-dev-tutorial.ring.intro
   (:require [ring.adapter.jetty :as jetty-adapter]))
 
+; 1. Write the request params to the response.
+; 2. Write "Welcome to FPDays to the response"
+; 3. Use response helpers. 
+; 4. Routing / => "Hello FP Days", anything else returns 404
+
+; 5. Static resources
+; 6. Middleware
+; 7. Parameter passing. 
+
+
 (defn handler
   [request]
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "Welcome to FPDays 2013"
+  {
+   :body (str request)
    })
 
 (defn -main [& args]
