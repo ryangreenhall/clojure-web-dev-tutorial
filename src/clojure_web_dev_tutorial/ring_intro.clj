a1d25292cc74cf2987dd76b52531bca88a1b8541
Ryan Greenhall <ryan.greenhall@gmail.com>
2013-09-29 14:33:23 +0100

    Providing a main function that will bootstrap the server

--- src/clojure_web_dev_tutorial/ring_intro.clj
+++ src/clojure_web_dev_tutorial/ring_intro.clj
@@ -1,7 +1,13 @@
-(ns clojure-web-dev-tutorial.ring-intro)
+(ns clojure-web-dev-tutorial.ring-intro
+  (:require [ring.adapter.jetty :as jetty-adapter]))
+
+(defn handler
+  [request]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Welcome to FPDays 2013"
+   })
 
 (defn -main [& args]
   (println "An introduction to Ring")
-  ;; (jetty/run-jetty app {:port 8888})
-  
-  )
+  (jetty-adapter/run-jetty handler {:port 8888}))
