c47629c762da0aa4358c3096130083d1b481dca9
Ryan Greenhall <ryan.greenhall@gmail.com>
2013-09-29 14:42:38 +0100

    Remove libs that are not needed for the initial ring intro

--- project.clj
+++ project.clj
@@ -5,11 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-core "1.2.0"]
-                 [ring/ring-jetty-adapter "1.1.0"]
-                 [ring-basic-authentication "1.0.0"]
-                 [compojure "1.0.4"]
-                 [hiccup "1.0.3"]
-                 [org.clojure/data.json "0.1.3"]
-                 [clj-http "0.4.3" :exclusions [commons-codec]]]
+                 [ring/ring-jetty-adapter "1.1.0"]]
+                 
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler clojure-web-dev-tutorial.hello-world/handler})
