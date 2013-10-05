b36add3036d6041f7cefbbaf21771696d7c31914
Ryan Greenhall <ryan.greenhall@gmail.com>
2013-10-05 14:21:15 +0100

    Introducing enlive to parse responses from the app using css selectors

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
                  [ring "1.2.0"]
-                 [ring-mock "0.1.5"]]
+                 [ring-mock "0.1.5"]
+                 [enlive "1.1.4"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler clojure-web-dev-tutorial.ring.intro/handler})
--- test/clojure_web_dev_tutorial/app_test.clj
+++ test/clojure_web_dev_tutorial/app_test.clj
@@ -1,8 +1,10 @@
 (ns clojure-web-dev-tutorial.app-test
   (:use clojure.test
+        net.cgrand.enlive-html
         ring.mock.request
         clojure-web-dev-tutorial.app))
 
 (deftest the-app
   (testing "the root says hello"
+    (html-resource (html-snippet "<html></html>"))
     (is (= "<h1>Hello World</h1>" (:body (app (request :get "/")))))))
