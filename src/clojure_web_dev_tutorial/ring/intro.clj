62af0da1871cd45aa88f66dec1490a2e487d7f61
Ryan Greenhall <ryan.greenhall@gmail.com>
2013-10-23 09:06:27 +0100

    Excercise descriptions are now inline with the solutions

--- src/clojure_web_dev_tutorial/ring/intro.clj
+++ src/clojure_web_dev_tutorial/ring/intro.clj
@@ -2,31 +2,22 @@
   (:require [ring.adapter.jetty :as jetty-adapter]
             [ring.middleware.resource :as resource]))
 
-; 1. Write the request params to the response.
-
-; 2. Write "Welcome to FPDays to the response"
-;    - Set status to 200
-;    - Set Content-Type to "text/plain"
 
 ; 3. Use response helpers. 
 
-; 4. Routing / => "Hello FP Days", anything else returns 404
-
-; 5. Middleware
-;    - Write a simple middleware function that logs the request to the console.
-
-; 6. Static resources
-;    - Configure the app to serve the css in the public dir. Hint: resource middleware
 
 ; 7. Parameter passing
 
-
+; 1. Write the request params to the response.
 (defn request-in-response-handler
   [request]
   {
    :body (str request)
    })
-   
+
+; 2. Write "Welcome to FPDays to the response"
+;    - Set status to 200
+;    - Set Content-Type to "text/plain"   
 (defn welcome-to-fp-days-handler
   [request]
   {
@@ -34,7 +25,8 @@
     :headers {"Content-Type" "text/plain"}
     :body (str "Welcome to FPDays")
    })
-   
+
+; 4. Routing / => "Hello FP Days", anything else returns 404   
 (defn routing-handler
   [request]
   (let [uri (:uri request)]
@@ -44,10 +36,9 @@
         {:status 404
          :body "Not Found!"})))
    
-(def app-with-resources
-  (-> welcome-to-fp-days-handler
-      (resource/wrap-resource "public")))
-      
+
+; 5. Middleware
+;    - Write a simple middleware function that logs the request to the console.      
 (defn wrap-logging
   [handler]
   (fn [request]
@@ -57,6 +48,13 @@
 (def logging-enabled-welcome-app
   (-> welcome-to-fp-days-handler
       wrap-logging))
+
+; 6. Static resources
+;    - Configure the app to serve the css in the public dir. Hint: resource middleware
+(def app-with-resources
+  (-> welcome-to-fp-days-handler
+      (resource/wrap-resource "public")))
+
 
 (defn -main [& args]
   (println "Starting server...")
