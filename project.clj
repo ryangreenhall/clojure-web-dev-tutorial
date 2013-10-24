26e9c686a4f3e651f863f67728eb246b4a7d5452
Ryan Greenhall <ryan.greenhall@gmail.com>
2013-10-24 07:14:03 +0100

    Fix incorrect handler name given to ring handler

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
                  [ring/ring-core "1.2.0"]
                  [ring/ring-jetty-adapter "1.1.0"]]            
   :plugins [[lein-ring "0.8.5"]]
-  :ring {:handler clojure-web-dev-tutorial.ring.intro/handler})
+  :ring {:handler clojure-web-dev-tutorial.ring.intro/request-in-response-handler})
